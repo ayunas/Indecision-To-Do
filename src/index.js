08124676f3c17b98ff715b2d12578be74f727c43
Amir Yunas <ayunas.py@gmail.com>
2019-03-28 11:36:26 -0500

    dynamically render todos
    in the template, dynamically rendered <p> tags for each todo stored in the app.todos object array property.  return a p tag with textContent of the todo.  Added label in the <p> tag "Todo Count: "

--- src/index.js
+++ src/index.js
@@ -28,16 +28,20 @@ const removeAll = () => {
   console.log(app.todos);
 };
 
+const numbers = [1, 2, 3, 4];
 const renderTemplate = () => {
   const template = (
     <div>
       <h1>Indecision - ToDo</h1>
-      <p>{app.todos.length > 0 ? "Todos:" : ""}</p>
-      <p>{app.todos.length}</p>
+      <p>{app.todos.length > 0 ? "Todos:" : "You have no todos!"}</p>
+      <p>Todo Count: {app.todos.length}</p>
       <form onSubmit={addTodo}>
         <input type="text" name="todo" />
         <button>Add</button>
       </form>
+      {app.todos.map(todo => (
+        <p>{todo}</p>
+      ))}
       <button onClick={removeAll}>Remove All</button>
     </div>
   );
