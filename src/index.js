caee660813a529c1c34c5b6d64c886e784d17c7d
Amir Yunas <ayunas.py@gmail.com>
2019-03-28 09:53:23 -0500

    Render Counter
    in addTodo,  got the value of the input by event.target.elements."name".value.   if that exists, push it to an array property of our app object.   And put the rendering of the template into a function, and call that function each time a new todo list is added.  because React works with a virtual DOM, the real DOM is not rerendered.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+const app = {
+  name: "Indecision",
+  subtitle: "Organize your Life",
+  todos: []
+};
+
 const rootElement = document.getElementById("root");
 const addTodo = e => {
   e.preventDefault();
-  console.log("added");
+  const todo = e.target.elements.todo.value;
+  console.log(e.target.elements.todo.value);
+
+  if (todo) {
+    console.log(todo.value);
+    app.todos.push(todo.value);
+    renderTemplate();
+  }
 };
 
-const template = (
-  <div>
-    <h1>Indecision - ToDo</h1>
-    <form onSubmit={addTodo}>
-      <input type="text" />
-      <button>Add</button>
-    </form>
-  </div>
-);
+const renderTemplate = () => {
+  const template = (
+    <div>
+      <h1>Indecision - ToDo</h1>
+      <p>{app.todos.length}</p>
+      <form onSubmit={addTodo}>
+        <input type="text" name="todo" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, rootElement);
+};
 
-ReactDOM.render(template, rootElement);
+renderTemplate();
